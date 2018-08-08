53de6a75c64b6271f976598f1542be7a7cfccd8f
frisaac <frisaac@stu.naperville203.org>
2018-08-08 20:40:41 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,13 +1,13 @@
 
 #def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
-  summer_olympics = {
+summer_olympics = {
   :Sydney => "2000",
   :Athens => "2004",
   :Beijing => "2008",
   :London => "2012"
-  }
-  puts summer_olympics
+}
+puts summer_olympics
 #end
 #create_olympics_hash
 
