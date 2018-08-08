574838f9042252edcb81ca7ca7fb4e1ff5238612
frisaac <frisaac@stu.naperville203.org>
2018-08-08 20:43:49 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -7,33 +7,33 @@ summer_olympics = {
   :Beijing => "2008",
   :London => "2012"
 }
-puts summer_olympics
-#end
-#create_olympics_hash
+# puts summer_olympics
+# #end
+# #create_olympics_hash
 
-def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-  summer_olympics[:Atlanta] = "1996"
-end
-add_a_key_value_pair
+# def add_a_key_value_pair
+#   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+#   summer_olympics[:Atlanta] = "1996"
+# end
+# add_a_key_value_pair
 
-def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-  summer_olympics.each do |x,y|
-    puts "The #{x} summer olympics took place in #{y}."
-  end
-end
-iterate_through_hash
+# def iterate_through_hash
+#   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
+#   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+#   summer_olympics.each do |x,y|
+#     puts "The #{x} summer olympics took place in #{y}."
+#   end
+# end
+# iterate_through_hash
 
-upcased_cities = []
-def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
-  summer_olympics.each do |x,y|
-    z = y.upcase
-    upcased_cities << z
-  end
-  puts upcased_cities
-end
-iterate_through_keys
+# upcased_cities = []
+# def iterate_through_keys
+#   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
+#   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+#   summer_olympics.each do |x,y|
+#     z = y.upcase
+#     upcased_cities << z
+#   end
+#   puts upcased_cities
+# end
+# iterate_through_keys
