f08f9a375654fc491b67437374d08ac73e952a99
frisaac <frisaac@stu.naperville203.org>
2018-08-08 20:47:08 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -10,8 +10,7 @@ summer_olympics = {
 
 end 
 puts summer_olympics
-
-# #create_olympics_hash
+create_olympics_hash
 
 # def add_a_key_value_pair
 #   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
